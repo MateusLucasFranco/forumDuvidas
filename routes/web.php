d1ab9fd892eee73a4b7655c0a05096a0ef4e2595
MateusLucasFranco <mateusllucasfranco@gmail.com>
2024-01-03 17:31:02 -0300

    new routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\Admin\SupportController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+
+
+Route::get('/contato', [SiteController::class, 'contact']);
 
 Route::get('/', function () {
     return view('welcome');
