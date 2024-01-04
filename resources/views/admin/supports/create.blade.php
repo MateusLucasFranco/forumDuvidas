52a700d077530e18cbf6daf1e6061dec896f948b
MateusLucasFranco <mateusllucasfranco@gmail.com>
2024-01-04 17:24:49 -0300

    paginas create e edit estilizada e estruturada

---
+++ resources/views/admin/supports/create.blade.php
@@ -0,0 +1,12 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Criar Novo Tópico')
+@section('header')
+    <h1 class="text-lg text-black-500">Nova Dúvida</h1>
+@endsection
+
+@section('content')
+    <form action="{{ route('supports.store') }}" method="POST">
+        @include('admin.supports.partials.form')
+    </form>
+@endsection
