8f76e4fad0282bd029e04805766d306bd43b05c3
MateusLucasFranco <mateusllucasfranco@gmail.com>
2024-01-04 17:26:43 -0300

    add support migration e upd rotas

--- database/migrations/2014_10_12_100000_create_supports_table.php
+++ database/migrations/2014_10_12_100000_create_supports_table.php
@@ -12,9 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->id();
+            $table->string('subject');
+            $table->enum('status', ['a', 'p', 'c']);
+            $table->text('body');
+            $table->timestamps();
         });
     }
 
