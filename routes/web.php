7703824e631cb5b67585eccfb84b02b0d29e927f
MateusLucasFranco <mateusllucasfranco@gmail.com>
2024-01-05 17:18:57 -0300

    add novas rotas e correcoes em rotas antigas

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,31 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
-Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 Route::get('/contato', [SiteController::class, 'contact']);
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
 });
+
+Route::get('/dashboard', function() {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('/supports/{id}/replies', [ReplySupportController::class, 'store'])->name('replies.store');
+    Route::delete('/supports/{id}/replies/{reply}', [ReplySupportController::class, 'destroy'])->name('replies.destroy');
+    Route::get('/supports/{id}/replies', [RepliesSupportController::class, 'index'])->name('replies.index');
+
+    Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
+    Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
+    Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+    Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+    Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+    Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+});
+
+require __DIR__ . '/auth/php';
