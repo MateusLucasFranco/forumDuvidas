8240e811c1cc1421725cb244f1278d9da16916f7
MateusLucasFranco <mateusllucasfranco@gmail.com>
2024-01-17 10:35:43 -0300

    upd correoes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\SiteController;
-use App\Http\Controllers\Admin\SupportController;
+use App\Http\Controllers\Admin\{ReplySupportController, SupportController};
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,7 +32,7 @@ Route::middleware(['auth'])->group(function() {
 
     Route::post('/supports/{id}/replies', [ReplySupportController::class, 'store'])->name('replies.store');
     Route::delete('/supports/{id}/replies/{reply}', [ReplySupportController::class, 'destroy'])->name('replies.destroy');
-    Route::get('/supports/{id}/replies', [RepliesSupportController::class, 'index'])->name('replies.index');
+    Route::get('/supports/{id}/replies', [ReplySupportController::class, 'index'])->name('replies.index');
 
     Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
     Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
