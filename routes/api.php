37bc730c9ba6ba7a87fa10ebdab14f58df4d8f25
MateusLucasFranco <mateusllucasfranco@gmail.com>
2024-01-17 10:35:09 -0300

    auth sanctum e rotas de controle de autenticacao

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\ReplySupportApiController;
+use App\Http\Controllers\Api\SupportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +16,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/replies/{support_id}', [ReplySupportApiController::class, 'getRepliesFromSupport']);
+    Route::get('/replies/{support_id}', [ReplySupportApiController::class, 'createNewReply']);
+    Route::delete('/replies/{id}', [ReplySupportApiController::class, 'destroy']);
+
+    Route::apiResource('/supports', SupportController::class);
 });
